37d10f3e973e68f49335ed98981432e5dd88e904
Lydia Lee <63779148+ljy719@users.noreply.github.com>
2026-08-27 18:24:43 +0900

    mobile menu

--- script.js
+++ script.js
@@ -52,3 +52,27 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+// =========================================================
+// MOBILE MENU
+// =========================================================
+
+const mobileMenuButton =
+  document.getElementById("mobileMenuButton");
+
+const mobileMenu =
+  document.getElementById("mobileMenu");
+
+
+if (mobileMenuButton && mobileMenu) {
+
+  mobileMenuButton.addEventListener(
+    "click",
+    () => {
+
+      mobileMenu.classList.toggle("active");
+
+    }
+  );
+
+}
